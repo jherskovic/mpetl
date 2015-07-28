95d5672f7b12053d9cc8ed0196db14f1c6b7e6e0
Jorge Herskovic <jherskovic@gmail.com>
2015-07-28 15:57:40 -0500

    Added setup.py and .gitignore.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='mpetl',
+    version='1.0',
+    packages=['mpetl'],
+    url='https://github.com/jherskovic/mpetl',
+    license='Apache',
+    author='Jorge Herskovic',
+    author_email='jherskovic@gmail.com',
+    description='MultiProcessing ETL pipelines for embarrassingly parallel work.'
+)
