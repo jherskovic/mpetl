eb478c3c040534d12f5e559ae9beb2934a8c15e4
jrherskovic <jrherskovic@mdanderson.org>
2015-08-04 11:18:50 -0500

    Removed unnecessary unicode strings from setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
-from __future__ import absolute_import
 from distutils.core import setup
 
 setup(
-    name=u'mpetl',
-    version=u'1.0',
-    packages=[u'mpetl'],
-    url=u'https://github.com/jherskovic/mpetl',
-    license=u'Apache',
-    author=u'Jorge Herskovic',
-    author_email=u'jherskovic@gmail.com',
-    description=u'MultiProcessing ETL pipelines for embarrassingly parallel work.'
+    name='mpetl',
+    version='1.0',
+    packages=['mpetl'],
+    url='https://github.com/jherskovic/mpetl',
+    license='Apache',
+    author='Jorge Herskovic',
+    author_email='jherskovic@gmail.com',
+    description='MultiProcessing ETL pipelines for embarrassingly parallel work.'
 )
