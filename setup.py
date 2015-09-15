96647743d6dc8665a86a0685495169193d290ad7
Jorge Herskovic <jherskovic@gmail.com>
2015-09-15 09:13:20 -0500

    Fixed lingering Unicode string in setup.py of the Python 2 version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name=u'mpetl',
     version=u'1.0',
-    packages=[u'mpetl'],
+    packages=['mpetl'],
     url=u'https://github.com/jherskovic/mpetl',
     license=u'Apache',
     author=u'Jorge Herskovic',
