2e1beaa70b5a313bdcb19b7deec828caca2ac770
Jorge Herskovic <jherskovic@gmail.com>
2015-09-15 09:15:07 -0500

    Fixed version string in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='mpetl',
-    version='1.0',
+    version='1.1.1',
     packages=['mpetl'],
     url='https://github.com/jherskovic/mpetl',
     license='Apache',
